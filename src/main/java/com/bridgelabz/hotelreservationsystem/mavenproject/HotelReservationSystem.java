16d1ef407cff35d9cfa33123735ff17dc035c018
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-21 17:51:50 +0530

    Added: Printing Welcome Message of Project:- Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/mavenproject/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservationsystem.mavenproject;
+
+public class HotelReservationSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome in the Maven Project");
+		System.out.println();
+		System.out.println("Hotel Reservation System: Miami");
+	}
+}
